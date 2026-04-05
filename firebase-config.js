d65cb4469cc0b7e85fb0fa3d079b175527bc6bbf
KJY_GALAXYbook5 <rlawnsduq123@gmail.com>
2026-04-05 09:36:45 +0900

    Deploy updated checklist homepage

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration for Jeju Baby Checklist
+// 이 설정값은 비밀이 아니지만 참고용으로 저장
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJD5-euVt9RWJiXIRbWyPo9d_cIeeESMo",
+  authDomain: "webanything-466f5.firebaseapp.com",
+  databaseURL: "https://webanything-466f5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webanything-466f5",
+  storageBucket: "webanything-466f5.firebasestorage.app",
+  messagingSenderId: "60644884106",
+  appId: "1:60644884106:web:fa2497dc1854b67ea1b773"
+};
